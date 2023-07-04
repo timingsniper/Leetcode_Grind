298c3fd6fdcab48911cdcb502484da056849fa1a
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2023-07-04 09:42:51 +0800

    Update 1768. Merge Strings Alternately.js

--- Programming Skills/1768. Merge Strings Alternately.js
+++ Programming Skills/1768. Merge Strings Alternately.js
@@ -2,18 +2,13 @@
  * @param {string} word1
  * @param {string} word2
  * @return {string}
-   https://leetcode.com/problems/merge-strings-alternately/?envType=study-plan-v2&envId=programming-skills
+    https://leetcode.com/problems/merge-strings-alternately/description/
  */
 var mergeAlternately = function(word1, word2) {
     let len = word1.length >= word2.length ? word1.length : word2.length;
     let ans = "";
     for (let i = 0; i<len; i++) {
-        if(i < word1.length) {
-            ans += word1[i];
-        }
-        if(i < word2.length) {
-            ans += word2[i];
-        }
+       ans += (word1[i] || '') + (word2[i] || '');
     }
     return ans;
 };
