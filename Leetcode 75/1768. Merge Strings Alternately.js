5a06c99b19a4b62c4401e343e8b09b113912c03a
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2023-07-04 08:13:18 +0800

    Create 1768. Merge Strings Alternately.js

---
+++ Leetcode 75/1768. Merge Strings Alternately.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+   https://leetcode.com/problems/merge-strings-alternately/description/
+ */
+var mergeAlternately = function(word1, word2) {
+    let len = word1.length >= word2.length ? word1.length : word2.length;
+    let ans = "";
+    for (let i = 0; i<len; i++) {
+        if(i < word1.length) {
+            ans += word1[i];
+        }
+        if(i < word2.length) {
+            ans += word2[i];
+        }
+    }
+    return ans;
+};
